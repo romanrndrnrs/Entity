8377a4db7d0122d925006f6475df904b60cb72fe
romanrndrnrs <roman.randri@protonmail.com>
2022-04-02 17:16:15 +0200

    ERC 721 FULL

--- entityownership.sol
+++ entityownership.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8;
 
 import "./entityattack.sol";
-// import "./erc721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
-contract EntityOwnership is EntityAttack ERC721 {
+contract EntityOwnership is EntityAttack, ERC721Full {
     mapping (uint => address) entityApproval;
     function balanceOf(address _owner) external view returns (uint256);
     function ownerOf(uint256 _tokenId) external view returns (address);
